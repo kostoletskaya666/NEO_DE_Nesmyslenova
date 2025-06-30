f4c213be32a9e2f9de5505c749b7bf502b5d36e6
kostoletskaya666 <130798818+kostoletskaya666@users.noreply.github.com>
2025-06-30 14:12:30 +0300

    Delete ds directory

--- ds/logs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE LOGS.ETL_LOGS (
-    log_id SERIAL PRIMARY KEY,
-    process_name VARCHAR(100) NOT NULL,
-    start_time TIMESTAMP NOT NULL,
-    end_time TIMESTAMP,
-    status VARCHAR(20) CHECK (status IN ('STARTED', 'SUCCESS', 'FAILED')),
-    rows_processed INTEGER,
-    error_message TEXT
-);
